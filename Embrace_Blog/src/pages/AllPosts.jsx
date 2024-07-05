6a1ee682011521aadd83d4868f2f789d597f552d
Gaurav Kumar Singh <127982407+gaurav-singhh@users.noreply.github.com>
2024-07-05 23:44:32 +0530

    Updated  AllPosts.jsx format for smaller screens

--- Embrace_Blog/src/pages/AllPosts.jsx
+++ Embrace_Blog/src/pages/AllPosts.jsx
@@ -2,24 +2,28 @@ import React, { useState, useEffect } from "react";
 import { Container, PostCard } from "../components";
 import appwriteService from "../appwrite/config";
 import { useSelector } from "react-redux";
+
 function AllPosts() {
   const isDarkTheme = useSelector((state) => state.DarkMode.isDarkTheme);
-
   const [posts, setPosts] = useState([]);
 
-  appwriteService.getPosts([]).then((posts) => {
-    if (posts) {
-      setPosts(posts.documents);
-    }
-  });
+  useEffect(() => {
+    appwriteService.getPosts([]).then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+      }
+    });
+  }, []);
+
   return (
-    <div
-      className={`w-full  ${isDarkTheme ? "bg-gray-900" : "bg-white"}  py-2`}
-    >
+    <div className={`w-full ${isDarkTheme ? "bg-gray-900" : "bg-white"} py-2`}>
       <Container>
         <div className="flex flex-wrap">
           {posts.map((post) => (
-            <div key={post.$id} className="p-2 w-1/4">
+            <div
+              key={post.$id}
+              className="p-2 w-full sm:w-1/2 md:w-1/2 lg:w-1/4"
+            >
               <PostCard {...post} />
             </div>
           ))}
